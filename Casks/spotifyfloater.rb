33b011aacec6200754e1cfc81edb713849c3ead6
Laurynas Lukas Kanopka <115489378+LukasKanopka@users.noreply.github.com>
2026-04-08 13:38:59 -0400

    Update spotifyfloater to version 1.0.1

--- Casks/spotifyfloater.rb
+++ Casks/spotifyfloater.rb
@@ -1,6 +1,6 @@
 cask "spotifyfloater" do
-  version "1.0.0"
-  sha256 "0816a8ef95f5ae4524201bdd15abf94515a96a8f883c9a151abb427fcf3dd758"
+  version "1.0.1"
+  sha256 "a336f30f056b38c80271fe6cbe63a330aa8ba1b45ef2760ed38b6065d4823f0b"
 
   url "https://github.com/LukasKanopka/Floater-For-Spotify/releases/download/v#{version}/SpotifyFloater-v#{version}.zip"
   name "SpotifyFloater"
